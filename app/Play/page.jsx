002ad9b8b50f459a6382537eb71a7751a432f995
SM-BD <mavin323lol@gmail.com>
2023-11-11 03:01:58 -0600

    Comit

---
+++ app/Play/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='bg-black min-h-screen relative'>
+      <div className='bg-slate-600 absolute top-0 right-0 pr-4 mr-60 min-h-full w-1/4'>
+        <div className='text-center text-cyan-50 pt-20 pb-20'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nam quasi aperiam accusantium.
+          Ab vitae, nobis, neque assumenda voluptas cumque tempora itaque animi cum quo asperiores. Numquam
+          autem nulla a.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page
